e3ebf440b237f3fc72776f33250de96c4636b292
Guiosep <gtunqui@gmail.com>
2021-01-31 04:08:44 -0500

    refact front

--- src/app/laboratory/laboratory.component.ts
+++ src/app/laboratory/laboratory.component.ts
@@ -1,92 +1,93 @@
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
 import * as THREE from 'three';
+//import { LaboratorioService } from '../core/api'
 
-//declare const init:any;
+const ELEMENT_DATA = [
+  {
+  "idLab": 1,
+  "name": "Anatomía topográfica",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 2,
+  "name": "El Brazo",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 3,
+  "name": "Región del codo",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 4,
+  "name": "Antebrazo",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 5,
+  "name": "Región de la muñeca",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 6,
+  "name": "Columna Vertebral",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 7,
+  "name": "Médula espinal",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 8,
+  "name": "Región dorsal y nuca",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 9,
+  "name": "Anatomía de superficie",
+  "idCourse": 1,
+  "course": null
+  },
+  {
+  "idLab": 10,
+  "name": "Huesos de la cabeza",
+  "idCourse": 1,
+  "course": null
+  }
+];
 
 @Component({
   selector: 'app-laboratory',
   templateUrl: './laboratory.component.html'
 })
 export class LaboratoryComponent implements OnInit {
-/*
-callfun(){
-	init();
-}
-*/
-  constructor() {
-  	/*
 
-   	let ww = window.innerWidth;
-    let wh = window.innerHeight;
+  displayedColumns: string[] = ['idLab', 'name'];
+  dataSource = ELEMENT_DATA;
 
-      let renderer = new THREE.WebGLRenderer({canvas : document.getElementById('scene')});
-      renderer.setSize(ww,wh);
-
-      let scene = new THREE.Scene();
-
-      let camera = new THREE.PerspectiveCamera(50,ww/wh, 0.1, 10000 );
-      camera.position.set(0,0,500);
-      scene.add(camera);
-
-      //Add a light in the scene
-      let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
-      directionalLight.position.set( 0, 0, 350 );
-      directionalLight.lookAt(new THREE.Vector3(0,0,0));
-      scene.add( directionalLight );
-
-      //Load the obj file
-      this.loadOBJ();
-      */
-
-  }
+  constructor(
+        //private labServ: LaboratorioService
+    ){}
 
   ngOnInit() {	
-	//this.callfun();
-  }
-  /*
-
-  loadOBJ(){
-  	      //Manager from ThreeJs to track a loader and its status
-      var manager = new THREE.LoadingManager();
-      //Loader for Obj from Three.js
-      var loader = new THREE.OBJLoader( manager );
-      
-      //Launch loading of the obj file, addBananaInScene is the callback when it's ready 
-      loader.load( 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/banana.obj', this.addBananaInScene);
+    this.cargarLaboratorios()
   }
 
+  async cargarLaboratorios(){
 
-  addBananaInScene(object){
+    //let labs = await this.labServ.listarLaboratorios()
 
-      let banana = object;
-      //Move the banana in the scene
-      banana.rotation.x = Math.PI/2;
-      banana.position.y = -200;
-      banana.position.z = 50;
-      //Go through all children of the loaded object and search for a Mesh
-      object.traverse( function ( child ) {
-        //This allow us to check if the children is an instance of the Mesh constructor
-        if(child instanceof THREE.Mesh){
-          child.material.color = new THREE.Color(0X00FF00);
-          //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
-          child.geometry.computeVertexNormals();
-        }
-      });
-      //Add the 3D object in the scene
-      scene.add(banana);
-      this.render();
-
+    //console.log("labs-> ",labs)
   }
 
-  render(){
-  	   requestAnimationFrame(render);
-
-      //Turn the banana
-      banana.rotation.z += .01;
-
-      renderer.render(scene, camera);
-  }
-
-*/
 }
